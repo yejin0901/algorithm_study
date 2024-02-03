a480edf6b38adf302d8770707e6bafb63d928779
yejin0901 <61917664+yejin0901@users.noreply.github.com>
2024-02-03 19:15:01 +0900

    Create 13335 트럭.py

---
+++ yejin/week4/13335 트럭.py
@@ -0,0 +1,17 @@
+n, w, l = map(int, input().split())
+trucks = list(map(int, input().split()))
+
+
+bridge = [0]*w
+time = 0
+
+while bridge:
+    time+=1
+    bridge.pop(0)
+    if trucks:
+        if sum(bridge) + trucks[0] <= l:
+            bridge.append(trucks.pop(0))
+        else:
+            bridge.append(0)
+
+print(time)
